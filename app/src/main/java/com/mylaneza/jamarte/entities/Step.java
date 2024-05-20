2500d0824802a9b3cda4ac12afd243c7ad05a980
Ignacio Gonzalez <mylaneza@gmail.com>
2024-05-19 19:26:38 -0600

    Step Fix

--- app/src/main/java/com/mylaneza/jamarte/entities/Step.java
+++ app/src/main/java/com/mylaneza/jamarte/entities/Step.java
@@ -1,5 +1,7 @@
 package com.mylaneza.jamarte.entities;
 
+import androidx.annotation.NonNull;
+
 /**
  * Created by mylaneza on 07/07/2018.
  */
@@ -12,6 +14,7 @@ public class Step {
     public String descriptionFollower;
     public String videoFilePath;
 
+    @NonNull
     public String toString(){
         return name +","+ count +","+base+","+ descriptionLeader +","+ descriptionFollower;
     }
