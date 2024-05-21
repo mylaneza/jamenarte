fb6fe1297756fb9e396da124500593d07fcc254f
Ignacio Gonzalez <mylaneza@gmail.com>
2024-05-20 21:20:27 -0600

    QueryAct fixes

--- app/src/main/java/com/mylaneza/jamarte/QueryAct.java
+++ app/src/main/java/com/mylaneza/jamarte/QueryAct.java
@@ -15,7 +15,7 @@ public class QueryAct extends Activity {
 
     Spinner spinner;
 
-    String[] opciones;
+    String[] options;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,70 +23,66 @@ public class QueryAct extends Activity {
         spinner = (Spinner)findViewById(R.id.qf1);
         int parent = getIntent().getIntExtra("com.mylaneza.jamarte.PARENT",0);
         switch(parent){
-            case 0: //Sesiones
-                initOpcionesSesiones();
+            case 0:
+                initSessionsOptions();
                 break;
-            case 1: //Pasos
-                initOpcionesPasos();
+            case 1:
+                initStepsOptions();
                 break;
-            case 2: //Lecciones
-                initOpcionesLecciones();
+            case 2:
+                initLessonsOptions();
                 break;
             case 3:
-                initOpcionesAvances();
+                initProgressOptions();
                 break;
-            default:
-                initOpcionesSesiones();
         }
-
-
-        ArrayAdapter<String> oAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item , opciones );
+        ArrayAdapter<String> oAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item , options);
         oAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
         spinner.setAdapter(oAdapter);
     }
 
-    private void initOpcionesSesiones(){
-        opciones = new String[5];
-        opciones[0] = "Todos";
-        opciones[1] = "Bravo Hay Jam";
-        opciones[2] = "Jam en Arte";
-        opciones[3] = "Calavera Swing";
-        opciones[4] = "Jam en Italia";
+    private void initSessionsOptions(){
+        options = new String[5];
+        options[0] = "Todos";
+        options[1] = "Bravo Hay Jam";
+        options[2] = "Jam en Arte";
+        options[3] = "Calavera Swing";
+        options[4] = "Jam en Italia";
     }
 
-    private void initOpcionesAvances(){
-        opciones = new String[4];
-        opciones[0] = "Todos";
-        opciones[2] = "Jam en Arte";
-        opciones[3] = "Calavera Swing";
+    private void initProgressOptions(){
+        options = new String[4];
+        options[0] = "Todos";
+        options[2] = "Jam en Arte";
+        options[3] = "Calavera Swing";
     }
 
-    private void initOpcionesPasos(){
-        opciones = new String[11];
-        opciones[0] = "Todos";
-        opciones[1] = "Solo Jazz";
-        opciones[2] = "Charleston";
-        opciones[3] = "Lindy Hop";
-        opciones[4] = "Jive";
-        opciones[5] = "Collegiate Shag";
-        opciones[6] = "St. Louis Shag";
-        opciones[7] = "Balboa";
-        opciones[8] = "Jitterbug";
-        opciones[9] = "West Coast Swing";
-        opciones[10] = "Boogie Woogie";
+    private void initStepsOptions(){
+        options = new String[11];
+        options[0] = "Todos";
+        options[1] = "Solo Jazz";
+        options[2] = "Charleston";
+        options[3] = "Lindy Hop";
+        options[4] = "Jive";
+        options[5] = "Collegiate Shag";
+        options[6] = "St. Louis Shag";
+        options[7] = "Balboa";
+        options[8] = "Jitterbug";
+        options[9] = "West Coast Swing";
+        options[10] = "Boogie Woogie";
     }
 
-    private void initOpcionesLecciones(){
-        opciones = new String[4];
-        opciones[0] = "Todos";
-        opciones[1] = "Jam en Arte";
-        opciones[2] = "Lindy Hop Puebla";
-        opciones[3] = "Calavera Swing";
+    private void initLessonsOptions(){
+        options = new String[4];
+        options[0] = "Todos";
+        options[1] = "Jam en Arte";
+        options[2] = "Lindy Hop Puebla";
+        options[3] = "Calavera Swing";
     }
 
     public void ok(View v){
         Intent data = new Intent();
-        data.putExtra("com.mylaneza.jamarte.OPCION",opciones[spinner.getSelectedItemPosition()]);
+        data.putExtra("com.mylaneza.jamarte.OPCION", options[spinner.getSelectedItemPosition()]);
         setResult(RESULT_OK,data);
         finish();
     }
